cc225d98d08dbac4dc8654f59479473d700b0efc
Sidelane <36916365+Sidelane@users.noreply.github.com>
2020-02-03 16:17:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from modules.board import Board
 from modules.player import Player
@@ -17,26 +17,28 @@ def main():
         for i, player in enumerate(players):
             if player.has_turned == False:
                 val = input(f"{player.char}s Turn. Specify the Field on the Board you want to change: ")
-                if b.check_val(val) == "0":
-                    b.set_val(player, val)
-                    player.has_turned = True
-                    if i == 0:
-                        players[1].has_turned = False
-                    elif i == 1:
-                        players[0].has_turned = False
-                    b.draw()
-                    ret = b.check_if_won(player)
-                    if ret == player.char:
-                        print(f"Player {player.char} has won!")
-                        game = False
-                        break
-                    elif ret == "Tie":
-                        print("Tie!")
-                        game = False
-                        break
+                if int(val) <= 8:
+                    if b.check_val(val) == "0":
+                        b.set_val(player, val)
+                        player.has_turned = True
+                        if i == 0:
+                            players[1].has_turned = False
+                        elif i == 1:
+                            players[0].has_turned = False
+                        b.draw()
+                        ret = b.check_if_won(player)
+                        if ret == player.char:
+                            print(f"Player {player.char} has won!")
+                            game = False
+                            break
+                        elif ret == "Tie":
+                            print("Tie!")
+                            game = False
+                            break
+                    else:
+                        val = input(f"{player.char}s Turn. Specify the Field on the Board you want to change: ")
                 else:
-                    val = input(f"{player.char}s Turn. Specify the Field on the Board you want to change: ")
-
+                    print("[-] Please pick a Value between 0-8")
 
 if __name__ == "__main__":
     main()
